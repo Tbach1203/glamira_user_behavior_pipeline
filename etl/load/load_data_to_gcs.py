022c15876296cde0eaa06d5705ca45a2bff19d72
Bách Trần <maitranbach2812@gmail.com>
2026-03-23 21:23:28 +0700

    update task export to gcs

--- etl/load/load_data_to_gcs.py
+++ etl/load/load_data_to_gcs.py
@@ -2,32 +2,33 @@ import os
 import json
 import logging
 from google.cloud import storage
-from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
-
 BUCKET_NAME = os.getenv("GCS_BUCKET")
-
 def get_gcs_client():
     return storage.Client()
 
-def upload_file_to_gcs(local_path, destination_blob):
+def upload_file_to_gcs(local_path, destination_name):
     try:
         client = get_gcs_client()
         bucket = client.bucket(BUCKET_NAME)
-        blob = bucket.blob(destination_blob)
+        blob = bucket.blob(destination_name)
         blob.upload_from_filename(local_path)
-        logging.info(f"Uploaded {local_path} → gs://{BUCKET_NAME}/{destination_blob}")
+        logging.info(f"Uploaded {local_path} → gs://{BUCKET_NAME}/{destination_name}")
     except Exception as e:
         logging.error(f"Upload failed for {local_path}: {e}")
         raise
 
-def mongo_to_jsonl(collection, blob):
+def mongo_to_jsonl(collection, blob_name):
     total = 0
     try:
+        client = get_gcs_client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(blob_name)
         if blob.exists():
             logging.info(f"Skip Mongo export (already exists): {blob.name}")
             return 0
+
         cursor = collection.find({}, no_cursor_timeout=True)
         with blob.open("wb") as f:
             for doc in cursor:
@@ -42,15 +43,11 @@ def mongo_to_jsonl(collection, blob):
         raise
 
 def export_to_gcs(ip_path, product_path, db):
-    logging.info("Start exporting raw data to GCS")
-    client = get_gcs_client()
-    bucket = client.bucket(BUCKET_NAME)
-    today = datetime.now().strftime("%Y-%m-%d")
-    upload_file_to_gcs(ip_path,f"processed/ip_locations/dt={today}/ip_locations.jsonl")
-    upload_file_to_gcs(product_path,f"processed/products/dt={today}/product_info.jsonl")
+    logging.info("Start exporting data to GCS")
+    upload_file_to_gcs(ip_path, f"ip_locations.jsonl")
+    upload_file_to_gcs(product_path, f"product_info.jsonl")
     logging.info("Exporting MongoDB collection: summary")
-    blob = bucket.blob(f"raw/glamira_raw.jsonl")
     collection = db["summary"]
-    total = mongo_to_jsonl(collection, blob)
-    logging.info(f"Exported {total} records → gs://{BUCKET_NAME}/raw/glamira_raw.jsonl")
+    total = mongo_to_jsonl(collection, f"glamira_raw.jsonl")
+    logging.info(f"Exported {total} records → gs://{BUCKET_NAME}/glamira_raw.jsonl")
     logging.info("Export pipeline completed successfully")
