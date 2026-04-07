79807ea2945022fe38ecb3d78d6ba602b632059e
Bách Trần <maitranbach2812@gmail.com>
2026-04-07 16:48:08 +0700

    update table stg_glamira_raw

--- dbt_pipeline/models/staging/stg_glamira_raw.sql
+++ dbt_pipeline/models/staging/stg_glamira_raw.sql
@@ -1,2 +1,17 @@
-SELECT *
-FROM `glowing-market-485009-u8.raw_layer.glamira_raw`
+SELECT
+  ip AS ip_address,
+  user_agent,
+  resolution,
+  user_id_db AS user_id,
+  device_id,
+  store_id,
+  order_id,
+  cart_products.product_id AS product_id,
+  cart_products.amount AS order_qty,
+  cart_products.price AS order_total_amount,
+  cart_products.currency AS currency_code,
+  SAFE_CAST(local_time AS DATE) AS order_local_time,
+  time_stamp AS order_timestamp
+FROM `glowing-market-485009-u8.raw_layer.glamira_raw`,
+UNNEST(cart_products) AS cart_products
+WHERE collection = 'checkout_success'
