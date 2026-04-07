c23238b58ed703d45363a056f1a8c40ddc0d0b9b
Bách Trần <maitranbach2812@gmail.com>
2026-04-07 15:53:16 +0700

    update table stg_product_info

--- dbt_pipeline/models/staging/stg_product_info.sql
+++ dbt_pipeline/models/staging/stg_product_info.sql
@@ -1,3 +1,11 @@
 SELECT 
-    *
-FROM `glowing-market-485009-u8.raw_layer.product_info`
+  product_id,
+  name AS product_name,
+  category AS category_id,
+  category_name,
+  SAFE_CAST(price AS NUMERIC) AS unit_price,
+  SAFE_CAST(min_price AS NUMERIC) AS min_price,
+  SAFE_CAST(max_price AS NUMERIC) AS max_price,
+  qty AS product_qty,
+  TRIM(REGEXP_REPLACE(min_price_format, r'[\d,\.]', '')) as currency_symbol
+FROM `glowing-market-485009-u8.raw_layer.product_info` 
